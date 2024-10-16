5eb8e8fa3d9d7b4752bfd7103440aaab8385d020
Alexander Urrutia <matias04041994@gmail.com>
2024-10-16 18:38:05 -0300

    Refactor astro.config.mjs to add passthrough image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  output: "server",
+  adapter: vercel(),
+  image: {
+    service: passthroughImageService(),
+  },
 });
