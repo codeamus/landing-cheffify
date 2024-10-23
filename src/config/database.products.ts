03ed62649aa7fdb8b39026cbbf3c72113a9d2962
Alexander Urrutia <matias04041994@gmail.com>
2024-10-23 13:22:49 -0300

    Refactor "Platos.astro" to include fade-in animation for product cards, update transition name for product images, and include dynamic rendering of product cards and update links

--- src/config/database.products.ts
+++ src/config/database.products.ts
@@ -599,3 +599,63 @@ export const PRODUCTS = [
     alt: "Puré de Garbanzos - comida prepadara",
   },
 ];
+
+import IconArroz from "@/assets/images/icons/icon-arroces.svg";
+import IconCarnesBlancas from "@/assets/images/icons/icon-chicken.svg";
+import IconGuiso from "@/assets/images/icons/icon-cooking-pot.svg";
+import IconCremas from "@/assets/images/icons/icon-cremas.svg";
+import IconFish from "@/assets/images/icons/icon-fish.svg";
+import IconMasas from "@/assets/images/icons/icon-masas.svg";
+import IconMeat from "@/assets/images/icons/icon-meat.svg";
+
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: "Carnes",
+    slug: "carnes",
+    icon: IconMeat,
+    active: false,
+  },
+  {
+    id: 2,
+    name: "Guisos",
+    slug: "guisos",
+    icon: IconGuiso,
+    active: false,
+  },
+  {
+    id: 3,
+    name: "Carnes Blancas",
+    slug: "carnes-blancas",
+    icon: IconCarnesBlancas,
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Masas",
+    slug: "masas",
+    icon: IconMasas,
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Pescados y mariscos",
+    slug: "pescados-y-mariscos",
+    icon: IconFish,
+    active: false,
+  },
+  {
+    id: 6,
+    name: "Cremas",
+    slug: "cremas",
+    icon: IconCremas,
+    active: false,
+  },
+  {
+    id: 7,
+    name: "Arroces",
+    slug: "arroces",
+    icon: IconArroz,
+    active: false,
+  },
+];
